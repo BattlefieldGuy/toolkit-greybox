f85643f6ae27139e9a11ea1e47454b52aec9af4a
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-13 11:46:23 +0100

    add Interactor, RaycastInteractor

---
+++ Runtime/Interactions/Interactor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace NL.XRLab.Toolkit.Greybox.Interactions
+{
+	public abstract class Interactor : MonoBehaviour
+	{
+		/// <summary>
+		///    The layer mask defining which layers can be interacted with.
+		/// </summary>
+		[SerializeField] protected LayerMask _interactionLayerMask;
+
+		/// <summary>
+		///    Interact with the given interactable object.
+		/// </summary>
+		/// <param name="interactable">The interactable object to interact with.</param>
+		protected static void Interact(IInteractable interactable)
+		{
+			interactable.Interact();
+		}
+
+		/// <summary>
+		///    Attempt to interact with an interactable object.
+		/// </summary>
+		public abstract void AttemptInteract();
+	}
+}
---
+++ Runtime/Interactions/RaycastInteractor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace NL.XRLab.Toolkit.Greybox.Interactions
+{
+	public class RaycastInteractor : Interactor
+	{
+		[SerializeField] private float _maxInteractionDistance = 5f;
+
+#if UNITY_EDITOR
+		[SerializeField] private bool _drawDebugRay = true;
+#endif
+
+		public override void AttemptInteract()
+		{
+#if UNITY_EDITOR
+			if (_drawDebugRay)
+				Debug.DrawRay(transform.position, transform.forward * _maxInteractionDistance, Color.yellow, 1f);
+#endif
+			if (!Physics.Raycast(
+				    transform.position, transform.forward, out var hit, _maxInteractionDistance, _interactionLayerMask)
+			   )
+				return;
+
+#if UNITY_EDITOR
+			if (_drawDebugRay)
+				Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.green, 1f);
+#endif
+
+			var interactable = hit.collider.GetComponent<IInteractable>();
+			if (interactable != null)
+				Interact(interactable);
+		}
+	}
+}
