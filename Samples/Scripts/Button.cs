4146ea7c5bc9e9fd900d27874acfc27328afb3b9
MatthewKlijnstra <matthewklijnstra@gmail.com>
2026-03-05 11:15:10 +0100

    First test push
    Added the Button Prefab and script to the package

---
+++ Samples/Scripts/Button.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    /// <summary>
+    /// Simple button script for the greyboxing toolkit.
+    /// 
+    /// Drag the necessary events into the inspector and make sure to use the "Player" prefab or add coliders and Rigidbody's to the controllers with the "Player" tag for the button to work.
+    /// </summary>
+
+    public UnityEvent OnPress;
+
+    private bool isPressed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isPressed = true;
+                StartCoroutine(Animation());
+                OnPress.Invoke();
+            }
+        }
+    }
+
+    private IEnumerator Animation()
+    {
+        this.transform.localPosition = new Vector3(0.0f, 0.45f, 0.0f);
+        yield return new WaitForSeconds(0.5f);
+        this.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+        isPressed = false;
+    }
+
+    #region --- DEBUG ---
+    public void OnTrigger()
+    {
+        print("Button was Pressed");
+    }
+    #endregion
+}
