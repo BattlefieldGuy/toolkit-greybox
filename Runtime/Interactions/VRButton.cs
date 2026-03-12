1b4679705ab3fae5b816b326ca24bc74a1ba95fb
MatthewKlijnstra <matthewklijnstra@gmail.com>
2026-03-12 15:16:10 +0100

    VRButton.cs update
    revamped the code for a code review.

--- Runtime/Interactions/VRButton.cs
+++ Runtime/Interactions/VRButton.cs
@@ -2,43 +2,61 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Simple button script for the greyboxing toolkit.
+///
+/// Drag the necessary events into the inspector and make sure to use the "Player" prefab or
+/// add coliders and Rigidbody's to the controllers with the "Player" tag for the button to work.
+/// </summary>
 public class VRButton : MonoBehaviour
 {
-   /// <summary>
-   /// Simple button script for the greyboxing toolkit.
-   /// 
-   /// Drag the necessary events into the inspector and make sure to use the "Player" prefab or add coliders and Rigidbody's to the controllers with the "Player" tag for the button to work.
-   /// </summary>
+	/// <summary> Unity event used to call other scripts when the button is pressed. </summary>
+	[SerializeField]
+	private UnityEvent _onPress;
 
-   public UnityEvent OnPress;
+	[SerializeField, Tooltip("The distance that the button moves on press, Default is 0.05")]
+	private float _pressDepth = 0.05f;
 
-   private bool isPressed = false;
+	[SerializeField, Tooltip("The duration of the buttons 'animation' time")]
+	private float _pressDuration = 0.5f;
 
-   private void OnTriggerEnter(Collider other)
-   {
-	  if (!isPressed)
-	  {
-		 if (other.CompareTag("Player"))
-		 {
-			isPressed = true;
-			StartCoroutine(Animation());
-			OnPress.Invoke();
-		 }
-	  }
-   }
+	/// <summary> Rest heigt where the button always moves back to, Always starting position of object on start. </summary>
+	private float _restHeight;
 
-   private IEnumerator Animation()
-   {
-	  this.transform.localPosition = new Vector3(0.0f, 0.45f, 0.0f);
-	  yield return new WaitForSeconds(0.5f);
-	  this.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
-	  isPressed = false;
-   }
+	/// <summary> Used to prevent a button from being pressed multiple times at once, which could cause issues with the animation and event invocation. </summary>
+	private bool _isPressed = false;
 
-   #region --- DEBUG ---
-   public void OnTrigger()
-   {
-	  print("Button was Pressed");
-   }
-   #endregion
+	/// <summary> Start is only used for setting the restHeight in this script. </summary>
+	private void Start()
+	{
+		_restHeight = this.transform.localPosition.y;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (_isPressed)
+			return;
+
+		if (other.CompareTag("Player"))
+		{
+			_isPressed = true;
+			StartCoroutine(ButtonAnimation());
+			_onPress.Invoke();
+		}
+	}
+
+	/// <summary>
+	/// Basic button animation for visual feedback.
+	/// The Depth of press and duration of the animation can be set in the inspector.
+	/// </summary>
+	private IEnumerator ButtonAnimation()
+	{
+		// Moves button down to give visual feedback of being pressed, and wait the set duration.
+		this.transform.localPosition = new Vector3(0.0f, _pressDepth, 0.0f);
+		yield return new WaitForSeconds(_pressDuration);
+
+		// Resets button and bool to be activated again.
+		this.transform.localPosition = new Vector3(0.0f, _restHeight, 0.0f);
+		_isPressed = false;
+	}
 }
